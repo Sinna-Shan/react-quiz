f490d4a095141f765dbef9ace8c2cec979fbf479
Noah Shan <luth595@gmail.com>
2024-07-31 23:11:21 +0530

    added reducer and the json-server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payLoad, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error(`Invalid action ${action.type}`);
+  }
+}
+
 export default function App() {
-  return <div>
-    Hello React quiz!
-  </div>
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {} = state;
+
+  useEffect(function () {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payLoad: data });
+      } catch (err) { 
+        dispatch({ type: "dataFailed"})
+      }
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>question?</p>
+      </Main>
+    </div>
+  );
 }
--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,35 +1,54 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0, step: 1 }
+
+function reducer(state, action) { 
+  console.log(state, action);
+
+  switch (action.type) {
+    case 'inc':
+      return { ...state, count: state.count + state.step };
+    case 'dec':
+      return {...state, count: state.count - state.step}
+    case 'setCount':
+      return { ...state, count: action.payload };
+    case 'setStep':
+      return {...state, step: action.payload };
+    case 'reset':
+      return initialState;
+    default:
+      throw new Error('Invalid action');
+  }
+
+ }
+
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
-  // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date('2024-07-31');
   date.setDate(date.getDate() + count);
 
-  const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
-  };
+  function defineStep(e) {
+    dispatch({type: 'setStep', payload: Number(e.target.value)});
+  }
 
-  const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
-  };
+  function defineCount(e) {
+    dispatch({type: 'setCount', payload: Number(e.target.value)});
+  }
 
-  const defineCount = function (e) {
-    setCount(Number(e.target.value));
-  };
+  function dec() {
+    dispatch({type: 'dec'});
+  }
 
-  const defineStep = function (e) {
-    setStep(Number(e.target.value));
-  };
+  function inc() {
+    dispatch({type: 'inc'});
+  }
 
-  const reset = function () {
-    setCount(0);
-    setStep(1);
-  };
+  function reset() {
+    dispatch({type: 'reset'});
+  }
 
   return (
     <div className="counter">
